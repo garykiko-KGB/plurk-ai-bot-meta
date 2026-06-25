1a45fe69de2293f7c9b638c5f3176c045bfdcf17
garykiko-KGB <garykiko@gmail.com>
2026-06-25 11:33:32 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,6 @@
+# ======== 設定區 ========
+KEYWORDS = ['加班', '好累', '社畜', '下班', '肝', '爆肝', '累死', '想離職', '不想上班']
+REPLY_ONLY_TO_FRIENDS = True
+AUTO_ADD_FRIEND = True
+FRIEND_CACHE_UPDATE_INTERVAL = 600
 
