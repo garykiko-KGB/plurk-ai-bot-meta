cb66ba06e673885e308b7311e4b724f7f5c413d8
garykiko-KGB <garykiko@gmail.com>
2026-06-25 14:01:54 +0800

    Update fifa.py

--- modules/fifa.py
+++ modules/fifa.py
@@ -5,17 +5,30 @@ API_KEY = os.getenv("FOOTBALL_API_KEY")
 
 BASE_URL = "https://v3.football.api-sports.io"
 
+def get_worldcup_id():
 
-def test():
     headers = {
         "x-apisports-key": API_KEY
     }
 
-    url = f"{BASE_URL}/status"
-
-    response = requests.get(url, headers=headers)
+    response = requests.get(
+        f"{BASE_URL}/leagues?search=World Cup",
+        headers=headers
+    )
 
     print("HTTP:", response.status_code)
-    print(response.text)
+    print(response.json())
 
-    return response.status_code
+# def test():
+#    headers = {
+#        "x-apisports-key": API_KEY
+#    }
+#
+#    url = f"{BASE_URL}/status"
+#
+#    response = requests.get(url, headers=headers)
+#
+#    print("HTTP:", response.status_code)
+#    print(response.text)
+#
+#    return response.status_code
