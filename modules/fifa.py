fbb225cee56f6a6d6e173eec3862af0f445ac8c5
garykiko-KGB <garykiko@gmail.com>
2026-06-25 13:44:17 +0800

    Update fifa.py

--- modules/fifa.py
+++ modules/fifa.py
@@ -1,1 +1,21 @@
+import os
+import requests
 
+API_KEY = os.getenv("FOOTBALL_API_KEY")
+
+BASE_URL = "https://v3.football.api-sports.io"
+
+
+def test():
+    headers = {
+        "x-apisports-key": API_KEY
+    }
+
+    url = f"{BASE_URL}/status"
+
+    response = requests.get(url, headers=headers)
+
+    print("HTTP:", response.status_code)
+    print(response.text)
+
+    return response.status_code
