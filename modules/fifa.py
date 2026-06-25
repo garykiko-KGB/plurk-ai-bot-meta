a5d79d1f540507f831244af05d4be6b0624e7b13
garykiko-KGB <garykiko@gmail.com>
2026-06-25 14:27:17 +0800

    Update fifa.py

--- modules/fifa.py
+++ modules/fifa.py
@@ -7,21 +7,45 @@ BASE_URL = "https://v3.football.api-sports.io"
 
 WORLD_CUP_ID = 1
 
-# def get_worldcup_id():
+def get_fixtures():
+    headers = {
+        "x-apisports-key": API_KEY
+    }
 
+    response = requests.get(
+        f"{BASE_URL}/fixtures",
+        headers=headers,
+        params={
+            "league": WORLD_CUP_ID,
+            "season": 2026
+        }
+    )
+
+    print("HTTP:", response.status_code)
+
+    data = response.json()
+
+    for match in data["response"]:
+        home = match["teams"]["home"]["name"]
+        away = match["teams"]["away"]["name"]
+        date = match["fixture"]["date"]
+
+        print(date, home, "vs", away)
+
+# def get_worldcup_id():
 #    headers = {
 #        "x-apisports-key": API_KEY
 #    }
-
+#
 #    response = requests.get(
 #        f"{BASE_URL}/leagues?search=World Cup",
 #        headers=headers
 #    )
-
+#
 #    print("HTTP:", response.status_code)
-    
+#    
 #    data = response.json()
-
+#
 #    for league in data["response"]:
 #        print(
 #            league["league"]["id"],
