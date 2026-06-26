80f3cab76dce33013b592d7552f6d6201f7c2f7a
garykiko-KGB <garykiko@gmail.com>
2026-06-26 14:17:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -252,8 +252,6 @@ def callback():
 
 # ======== 啟動 ========
 if __name__ == '__main__':
-    bot_thread = threading.Thread(target=run_bot, daemon=True)
-    bot_thread.start()
-    
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT",10000))
+    threading.Thread(target=run_bot, daemon=True).start()
+    app.run(host="0.0.0.0", port=port)
