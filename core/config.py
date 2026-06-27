5ea7c32b416e9fc5502b9e8ed71fae444179163d
garykiko-KGB <garykiko@gmail.com>
2026-06-27 12:41:20 +0800

    Update config.py
    增加除錯開關

--- core/config.py
+++ core/config.py
@@ -1,6 +1,8 @@
-# ======== 設定區 ========
+# ===== 設定區 =====
 KEYWORDS = ['加班', '好累', '社畜', '下班', '肝', '爆肝', '累死', '想離職', '不想上班']
 REPLY_ONLY_TO_FRIENDS = True
 AUTO_ADD_FRIEND = True
 FRIEND_CACHE_UPDATE_INTERVAL = 600
 
+# ===== 除錯開關 =====
+DEBUG = False
