e3a8cadb3838c8f5031d52a84f747ea5b083d27d
garykiko-KGB <garykiko@gmail.com>
2026-06-28 14:14:53 +0800

    Update config.py
    新增設定

--- core/config.py
+++ core/config.py
@@ -1,8 +1,20 @@
-# ===== 設定區 =====
+# ===== 基本設定區 =====
+# 關鍵字
 KEYWORDS = ['加班', '好累', '社畜', '下班', '肝', '爆肝', '累死', '想離職', '不想上班']
+# 只回應好友
 REPLY_ONLY_TO_FRIENDS = True
+# 自動加好友
 AUTO_ADD_FRIEND = True
+# 更新好友清單時間間隔(秒)
 FRIEND_CACHE_UPDATE_INTERVAL = 600
+
+# ===== 冷卻設定區 ====
+# 發廢文冷卻時間間隔(秒)
+POST_COOLDOWN_NORMAL = 1800
+# 按表操課冷卻時間間隔(秒)
+POST_COOLDOWN_SCHEDULED = 0
+# 系統公告冷卻時間間閣(秒)
+POST_COOLDOWN_SYSTEM = 0
 
 # ===== 除錯開關 =====
 DEBUG = False
