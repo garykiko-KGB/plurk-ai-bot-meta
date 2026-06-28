ccebc0da57bbb1426dd6af5e2688d4444949d9b7
garykiko-KGB <garykiko@gmail.com>
2026-06-28 15:49:42 +0800

    Update publisher.py
    拿掉print檢查。

--- behavior/publisher.py
+++ behavior/publisher.py
@@ -11,29 +11,23 @@ from behavior.ratelimiter import (
 
 def publish(content):
     
-    print("publisher() 被呼叫", flush=True)
     log("publisher() 被呼叫")
 
     allow = can_post()
     
-    print(f"can_post = {allow}", flush=True)
     log(f"can_post = {allow}")
     
     if not allow:
-        print("RateLimiter：冷卻中，取消發文", flush=True)
         log("RateLimiter：冷卻中，取消發文")
         return False
 
     result = plurk_publish(content)
-    print("publisher() 發文了", flush=True)
     log("publisher() 發文了")
 
     if result:
-        print("publisher() 發文成功，更新冷卻時間", flush=True)
         log("publisher() 發文成功，更新冷卻時間")
         record_post()
     else:
-        print("publisher() 發文失敗", flush=True)
         log("publisher() 發文失敗")
 
     log(f"publisher() 完成：{result}")
